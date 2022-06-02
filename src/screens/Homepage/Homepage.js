e1d5e13aa0e02dcf7dcc9050038ca1f49751e0fd
dudself <didenurboyaci@gmail.com>
2022-06-03 01:13:31 +0300

    changes in stop/resume buttons

--- src/screens/Homepage/Homepage.js
+++ src/screens/Homepage/Homepage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState , useRef} from 'react';
 import { View, StyleSheet, Pressable, Text, TextInput } from 'react-native';
 import styles from './Homepage.style';
 import * as Speech from 'expo-speech';
@@ -6,44 +6,49 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 
 import Button from '../../components/Button';
 
+export default function Homepage() {
+  const [input, setInput] = React.useState('');
+  const isSpeaking = useRef(false);
 
-export default function Homepage(){
-
-  const [input, setInput] = React.useState("");
-  const [speaking, isSpeaking] = React.useState(false);
+  let i = 0;
 
   // Konuşma
   const speak = () => {
+    
     const options = {
-      language: "tr-TR",
-      rate: 1.05,
-      pitch: 0.9
+      language: 'tr-TR',
+      rate: 1.1,
+      pitch: 0.9,
     };
-    Speech.speak(input, options);
-    onStart:() => isSpeaking(true);
+
+    const prompt = input.split(" ");
+    isSpeaking.current = true;
+    
+    do {
+      Speech.speak(prompt[i], options);
+      i++;
+    } while (i < prompt.length && isSpeaking.current == true);
+    
+    isSpeaking.current = false;
+    i = 0;
+
   };
 
-  // Durdur & kaldığın yerden devam et 
+  // Durdur & kaldığın yerden devam et
   const pauseResume = () => {
-    if(isSpeaking == true) {
-      Speech.pause();
-      onPause:() => isSpeaking(false);
-    }
-    else {
-      Speech.resume();
-      onResume:() => isSpeaking(true);
-    }
+    if(isSpeaking.current == true) {
+      isSpeaking.current = false;
+    } else if (i > 0 && isSpeaking.current == false ) {
+      isSpeaking.current = true;
+      speak();
+    } 
   };
 
-
-   return (
-
-   <View style={styles.container}>
-
-    <TextInput style={styles.input} onChangeText={(text) => setInput(text)} />
-    <Button onPress={speak} content= {'ios-megaphone'}/>
-    <Button onPress={pauseResume} content={'stop-circle-sharp'}/>
-
- </View>
- );
+  return (
+    <View style={styles.container}>
+      <TextInput style={styles.input} onChangeText={(text) => setInput(text)} />
+      <Button onPress={speak} content={'ios-megaphone'} />
+      <Button onPress={pauseResume} content={'stop-circle-sharp'} />
+    </View>
+  );
 }
